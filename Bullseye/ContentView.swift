3b09f4c9c21514d63065c519ad566f9a4ae0469d
Mehdi Ijadnazar <ijadnazar.mahdi@gmail.com>
2020-04-20 22:56:26 +0430

    Completed getting started with SwuifUI

--- Bullseye/ContentView.swift
+++ Bullseye/ContentView.swift
@@ -9,17 +9,32 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var alertIsVisible: Bool = false
+    @State var knockknockIsVisible = false
+    
     var body: some View {
         VStack {
             VStack {
                 Text("Welcome to my first app")
                     .fontWeight(.semibold)
                     .foregroundColor(Color.green)
-                Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
+                Button(action: {
+                    print("Button pressed")
+                    self.alertIsVisible = true
+                }) {
                     Text("Click Me")
+                }.alert(isPresented: $alertIsVisible) { () -> Alert in
+                    return Alert(title: Text("Hello there"), message: Text("This is my first pop-up"), dismissButton: .default(Text("Awesome")))
+                }
+                Button(action: {
+                    self.knockknockIsVisible = true
+                }) {
+                    Text(/*@START_MENU_TOKEN@*/"Knock Knock"/*@END_MENU_TOKEN@*/)
+                }.alert(isPresented: $knockknockIsVisible) { () -> Alert in
+                    return Alert(title: Text("Knock Knock"), message: Text("Who is there?"), dismissButton: .default(Text("dismiss")))
                 }
             }
-            
         }
     }
 }
